8e529982c8fd1d0ed82db37d8ed54b27f65077e4
JoeJoe1989 <josephzhou2011@gmail.com>
2014-11-08 17:01:42 -0500

    alapps modified

--- app/models/alapp.rb
+++ app/models/alapp.rb
@@ -1,4 +1,11 @@
 class Alapp < ActiveRecord::Base
   mount_uploader :driver_license_file, DriverLicenseFileUploader
+  mount_uploader :document1, Document1Uploader
+  mount_uploader :document2, Document2Uploader
+  mount_uploader :document3, Document3Uploader
+  mount_uploader :document4, Document4Uploader
+  mount_uploader :document5, Document5Uploader
+
+
   has_many :employments
 end
