0aa77e370ee2c06b2b498f79c5ee331d7b505e1a
JoeJoe1989 <josephzhou2011@gmail.com>
2014-11-19 19:39:08 -0500

    cbpapp validation added

--- app/helpers/cbpapps_helper.rb
+++ app/helpers/cbpapps_helper.rb
@@ -1,2 +1,27 @@
 module CbpappsHelper
+  class PhoneValidator < ActiveModel::Validator
+    def validate(record)
+      options[:fields].each do |field|
+        phone = record.send(field)
+        unless phone.blank?
+          phone_formats = [
+              /^\d{10}/,
+              /^\(\d\d\d\) \d\d\d-\d\d\d\d/,
+              /^\d\d\d\.\d\d\d\.\d\d\d\d/,
+              /^\d\d\d\-\d\d\d\-\d\d\d\d/
+          ]
+          valid = false
+          phone_formats.each do |format|
+            if phone.match( format )
+              valid = true
+            end
+          end
+          unless valid
+            record.errors.add(field, "Format of #{field} isn't recognized")
+          end
+        end
+      end
+    end
+
+  end
 end
