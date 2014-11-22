566061d981ec876754c302d7ed81388665cd1458
dichenli <dichenli@sas.upenn.edu>
2014-11-22 10:47:08 -0500

    fix and improve main page

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# <<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20141120040224) do
-# =======
-# ActiveRecord::Schema.define(version: 20141120031421) do
-# >>>>>>> b6901c7e0c09a32a0c5c46ee8adb75f2f901bec4
 
   create_table "alapps", force: true do |t|
     t.string   "first"
