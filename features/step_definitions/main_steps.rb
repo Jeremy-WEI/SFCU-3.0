9918e5ce7719d97d6d5e738869d371c071ca691e
Wu Jingyuan <katiewu0304@gmail.com>
2014-11-22 13:21:24 -0500

    cucumber test

---
+++ features/step_definitions/main_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I'm on the main page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I click the CBP link and choose some forms$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see export page$/) do
+  pending # express the regexp above with the code you wish you had
+end
