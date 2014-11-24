0179271497fcb35631065d9736b85efd39e4520f
dichenli <dichenli@sas.upenn.edu>
2014-11-23 23:56:15 -0500

    lob validate

--- app/models/alapp.rb
+++ app/models/alapp.rb
@@ -8,48 +8,49 @@ class Alapp < ActiveRecord::Base
   mount_uploader :document3, NormalFileUploader
   mount_uploader :document4, NormalFileUploader
   mount_uploader :document5, NormalFileUploader
-
-
-  validates :driver_license_file,
-            presence: true,
-            :file_size => {
-                :maximum => 5.megabytes.to_i
-            }
-  validates :document1, :document3, :document4, :document5, :document2,
-            :file_size => {
-                :maximum => 5.megabytes.to_i
-            }
-
-
-  has_many :employments
-
-  SSN_FORMAT = /\A\d{9}\z/
-  EMAIL_FORMAT = /\A[^@\s]+@(?:\w+\.)+[a-z]{2,}\z/i
-  PHONE_FORMAT =/\A\(?\d{3}[-\.)]?\d{3}[-\.]?\d{4}\z/
-
-  validates :first, :last, :alumni, :mother_maiden, :credit_req_type,
-            :term, :vehicle_condition, :name_nearest_relative, :phone_nearest_relative,
-            :driver_lisence_num, :signature, :today_date,
-            presence: true
-  validate :check_dob
-  validates :ssn, format: {with: SSN_FORMAT}
-
-  validates :amount_req, numericality: { greater_than_or_equal_to: 0.0}
-
-  validate :validates_vehicle_price_range
-  validates :price_range_min, numericality: { greater_than_or_equal_to: 0.0}
-  validates :price_range_max, numericality: { greater_than_or_equal_to: :price_range_min}
-  validate :validates_vehicle_type
-
-  validates :e_mail, format: {with: EMAIL_FORMAT}
-  validates :phone_number, :phone_nearest_relative, format: {with: PHONE_FORMAT}
-
-
-  validates :employ1_grosspay, :employ2_grosspay, :employ3_grosspay,
-            :account1_current_balance, :account2_current_balance, :account3_current_balance, :account4_current_balance,
-            :property1_market_val, :property2_market_val,
-            :rent_housing, :food, :utilities, :phone_bill, :bursar_bill, :miscellaneous,
-            numericality: {greater_than_or_equal_to: 0.0}, allow_nil: true
+  before_validation :make_perm_address
+  #
+  # validates :driver_license_file,
+  #           presence: true,
+  #           :file_size => {
+  #               :maximum => 5.megabytes.to_i
+  #           }
+  # validates :document1, :document3, :document4, :document5, :document2,
+  #           :file_size => {
+  #               :maximum => 5.megabytes.to_i
+  #           }
+  #
+  #
+  # has_many :employments
+  #
+  # SSN_FORMAT = /\A\d{9}\z/
+  # EMAIL_FORMAT = /\A[^@\s]+@(?:\w+\.)+[a-z]{2,}\z/i
+  # PHONE_FORMAT =/\A\(?\d{3}[-\.)]?\d{3}[-\.]?\d{4}\z/
+  #
+  # validates :first, :last, :alumni, :mother_maiden, :credit_req_type,
+  #           :term, :vehicle_condition, :name_nearest_relative, :phone_nearest_relative,
+  #           :driver_lisence_num, :signature, :today_date,
+  #           presence: true
+  # validate :check_dob
+  # validates :ssn, format: {with: SSN_FORMAT}
+  #
+  # validates :amount_req, numericality: { greater_than_or_equal_to: 0.0}
+  #
+  # validate :validates_vehicle_price_range
+  # validates :price_range_min, numericality: { greater_than_or_equal_to: 0.0}
+  # validates :price_range_max, numericality: { greater_than_or_equal_to: :price_range_min}
+  # validate :validates_vehicle_type
+  validate :validates_address
+  #
+  # validates :e_mail, format: {with: EMAIL_FORMAT}
+  # validates :phone_number, :phone_nearest_relative, format: {with: PHONE_FORMAT}
+  #
+  #
+  # validates :employ1_grosspay, :employ2_grosspay, :employ3_grosspay,
+  #           :account1_current_balance, :account2_current_balance, :account3_current_balance, :account4_current_balance,
+  #           :property1_market_val, :property2_market_val,
+  #           :rent_housing, :food, :utilities, :phone_bill, :bursar_bill, :miscellaneous,
+  #           numericality: {greater_than_or_equal_to: 0.0}, allow_nil: true
 
    validates :agree_terms, acceptance: true
 
@@ -80,17 +81,30 @@ class Alapp < ActiveRecord::Base
   end
 
   def validates_address
-    @username = "test_58d0bda464b7082a64604d18519f94ec619"
-    @lob = Lob.load(api_key: @username)
-    Lob.api_key = @username
-    @lob = Lob.load
+    #begin
+    # @username = "test_58d0bda464b7082a64604d18519f94ec619"
+    @lob = Lob.load(api_key: "test_58d0bda464b7082a64604d18519f94ec619")
     @result = @lob.addresses.verify(
-        address_line1: local_address_line1,
-        address_line2: local_address_line2,
-        city: local_address_city,
-        state: local_address_state,
-        zip: local_address_zip
+        address_line1: :local_address_line1,
+        address_line2: :local_address_line2,
+        city: :local_address_city,
+        state: :local_address_state,
+        zip: :local_address_zip
     )
+    #rescue
+      # errors.add(:local_address_line1, "Invalid address")
+    #end
+  end
+
+  def make_perm_address
+    if same == 1
+      self.perm_address_line1 = self.local_address_line1
+      self.perm_address_line2 = self.local_address_line2
+      self.perm_address_city = self.local_address_city
+      self.perm_address_state = self.local_address_state
+      self.perm_address_zip = self.local_address_zip
+      self.perm_country = self.local_country
+    end
   end
 
 end
