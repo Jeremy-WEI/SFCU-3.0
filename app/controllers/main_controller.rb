62ced21077ca4162a130b79f629de34ba74ae82f
dichenli <dichenli@sas.upenn.edu>
2014-12-06 15:43:03 -0500

    adjust authen

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,5 @@
 class MainController < ApplicationController
-  http_basic_authenticate_with :name => MainHelper.name, :password => MainHelper.password, only: :index
+  # http_basic_authenticate_with :name => MainHelper.name, :password => MainHelper.password, only: :index
 
   def index
     reset_session
