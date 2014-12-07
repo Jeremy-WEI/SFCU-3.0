c18ebfa28c079407bc22d21f228c5a4266fd5dd9
Wu Jingyuan <katiewu0304@gmail.com>
2014-12-07 16:40:34 -0500

    modify show

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203025309) do
+ActiveRecord::Schema.define(version: 20141203012822) do
 
   create_table "alapps", force: true do |t|
     t.string   "first"
